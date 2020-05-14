94ca79be06034d50c178e0875399081f9a2fe21d
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 20:19:53 +0000

    Add basic methods to Brand

--- lib/ethical_activewear/brand.rb
+++ lib/ethical_activewear/brand.rb
@@ -1,19 +1,28 @@
 require_relative "ethical_activewear.rb"
 
-class Brand 
+class EthicalActivewear::Brand 
   
   attr_reader :name, :url, :description, :rating, :shipping
+  @@all = []
+  
+  
   
   def create_from_page(brand_object)
     @name = brand_object.css("h2.layout-block__heading").text
-    @url = brand_object.css("h2.layout-block__heading a").attribute("href")
+    @url = brand_object.css("h2.layout-block__heading a").attribute("href").value
     @description = brand_object.css("div.layout-block__main div.layout-block__content.rich-text").text
     @rating = brand_object.css("div.layout-block__aside div.meta-labels").text
     @shipping = brand_object.css("div.layout-block__main div.layout-block__content figure.layout-block__image-container").text
-    
-    
+    save
   end
   
+  def save
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
   
   
   
--- lib/ethical_activewear/cli.rb
+++ lib/ethical_activewear/cli.rb
@@ -7,6 +7,8 @@ class EthicalActivewear::CLI
    puts "These are the top ethically-made activewear brands of 2020"
    puts "Which number brand would you like to learn more about?"
    puts ""
+   EthicalActivewear::Scraper.new.scrape_page
+   EthicalActivewear::Brand.all
   # Scraper class scrapes website 
   # Brand objects created
   # list brands
