f7cf963e6bc63081472a30c8e32f0616bc543c03
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 21:44:32 +0000

    Create loop

--- lib/ethical_activewear/cli.rb
+++ lib/ethical_activewear/cli.rb
@@ -5,21 +5,34 @@ class EthicalActivewear::CLI
  def call 
    puts "Welcome to your shop of ethically made activewear!"
    puts "These are the top ethically-made activewear brands of 2020"
-   puts "Which number brand would you like to learn more about?"
+   puts "Which brand would you like to learn more about?"
    puts ""
+   puts "Enter a number or exit"
    
    EthicalActivewear::Scraper.new.scrape_page
    
-   list_brands
-  
-  index = input_to_index(gets)
-  
-  print_brand(index)
-  
-  # print brand detail
-  # present new option
-  
+   start
+
  end
+ 
+ def start 
+   "Enter a number or exit."
+   list_brands
+   user_input = gets.strip
+   if user_input == 'exit'
+     puts "Thanks!"
+     exit 
+   elsif user_input.to_i.between?(1,17)
+     index = input_to_index(user_input)
+     print_brand(index)
+     start
+   else 
+     puts "Sorry, don't understand"
+     start
+    end
+ end
+ 
+ 
  
  def list_brands
    EthicalActivewear::Brand.all.each.with_index(1) do |brand, index|
@@ -34,8 +47,18 @@ class EthicalActivewear::CLI
  
  def print_brand(index)
    brand = EthicalActivewear::Brand.all[index]
+   puts ""
    puts brand.name
+   puts brand.rating 
+   puts "" 
+   puts "Description:"
+   puts brand.description 
+   puts ""
+   puts brand.shipping 
+   puts "Shop: #{brand.url}"
+   puts ""
  end
+ 
  
   
 end
