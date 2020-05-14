3ffcda9936af2353a017f4a4c6cf3bf7f28654f4
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 03:11:16 +0000

    Scraper class

--- lib/ethical_activewear/scraper.rb
+++ lib/ethical_activewear/scraper.rb
@@ -2,9 +2,18 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
+class Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://goodonyou.eco/ultimate-guide-ethical-activewear/"))
+   def get_page
+      Nokogiri::HTML(open("https://goodonyou.eco/ultimate-guide-ethical-activewear/"))
+   end
+  
+  
+   def self.scrape_page
+    get_page.css("div.article__block .layout-block").each do |brand|
+      Brand.new.create_from_page(brand)
+    end
   end
 
 
+end
