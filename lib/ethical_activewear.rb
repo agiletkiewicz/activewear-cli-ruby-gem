9a92f5d67dc9e7a9fa88b0565cd6db7c47003acf
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 00:30:43 +0000

    first commit

---
+++ lib/ethical_activewear.rb
@@ -0,0 +1,6 @@
+require "ethical_activewear/version"
+
+module EthicalActivewear
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ethical_activewear/version.rb
@@ -0,0 +1,3 @@
+module EthicalActivewear
+  VERSION = "0.1.0"
+end
