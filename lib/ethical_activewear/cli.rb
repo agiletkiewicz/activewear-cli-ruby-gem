47ef319fd89a9a49eb2dd9c3cd930557bfddf9b2
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 20:49:28 +0000

    List brands

--- lib/ethical_activewear/cli.rb
+++ lib/ethical_activewear/cli.rb
@@ -7,13 +7,18 @@ class EthicalActivewear::CLI
    puts "These are the top ethically-made activewear brands of 2020"
    puts "Which number brand would you like to learn more about?"
    puts ""
+   
    EthicalActivewear::Scraper.new.scrape_page
-   EthicalActivewear::Brand.all
-  # Scraper class scrapes website 
-  # Brand objects created
+   
+   EthicalActivewear::Brand.all.each.with_index(1) do |brand, index|
+     puts "#{index}. #{brand.name}"
+   end
+
   # list brands
   # get user input 
   
  end
+ 
+ 
   
 end
