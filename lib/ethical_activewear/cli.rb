e6f1587ec866e0cd42626e234dcef928352f84d0
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-14 19:37:44 +0000

    Staring CLI class

--- lib/ethical_activewear/cli.rb
+++ lib/ethical_activewear/cli.rb
@@ -3,8 +3,15 @@ require_relative "ethical_activewear.rb"
 class EthicalActivewear::CLI
   
  def call 
-   puts "hello"
-   binding.pry
+   puts "Welcome to your shop of ethically made activewear!"
+   puts "These are the top ethically-made activewear brands of 2020"
+   puts "Which number brand would you like to learn more about?"
+   puts ""
+  # Scraper class scrapes website 
+  # Brand objects created
+  # list brands
+  # get user input 
+  
  end
   
 end
